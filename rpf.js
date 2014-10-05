a2755ddb3b06a3f7543378a8bebf6692c5b60a21
jph <jph@hawaii.edu>
2014-10-04 16:28:09 -1000

    cleanup

--- rpf.js
+++ rpf.js
@@ -3,14 +3,17 @@
 (function() { // self-invoking anonymous function
   // all code goes here
 
-var userChoice;
-var compChoice;
+var userChoice,
+    compChoice,
+    userWins = 0,
+    compWins = 0
+    ;
 
 // noprotect
 function userChooses() {
   
   do {
-    userChoice = prompt("Select [R]ock, [P]aper or [F]ireball ('R', 'P', or 'F'):");
+    userChoice = prompt("Select [R]ock, [P]aper or [F]ireball:","Type 'R', 'P', or 'F'");
     if ((userChoice.toUpperCase() !== "R") && (userChoice.toUpperCase() !== "P") && (userChoice.toUpperCase() !== "F")) {
       alert("Try using R, P, or F. No messing around!");
     }
@@ -42,45 +45,48 @@ function result() {
   var userPick = userChooses();
   var compPick = computerChooses();
   
-  //display("User: " + userPick);
-  //display("computer: " + compPick);
-  
   switch(userPick) {
     case "R":
       display("User: " + userPick);
-      display("computer: " + compPick);
+      display("Computer: " + compPick);
       if(compPick === "P") {
         display("Paper covers Rock");
         display("Computer Wins!");
+        compWins++;
       } else if(compPick === "F") {
         display("Rock blocks Fireball");
         display("User Wins!");
+        userWins++;
       } else if(compPick === "R") {
         display("Tied! User and Computer both picked Rock.");
       }
       break;
     case "P":
       display("User: " + userPick);
-      display("computer: " + compPick);
+      display("Computer: " + compPick);
       if(compPick === "R") {
         display("Paper covers Rock");
         display("User Wins!");
+        userWins++;
       } else if(compPick === "F") {
         display("Fireball engulfs Paper");
         display("Computer Wins!");
+        compWins++;
       } if(compPick === "P") {
         display("Tied! User and Computer both picked Paper.");
       }
       break;
     case "F":
       display("User: " + userPick);
-      display("computer: " + compPick);      
+      display("Computer: " + compPick);      
       if(compPick === "R") {
         display("Rock blocks Fireball");
         display("Computer Wins!");
+        compWins++;
       } else if(compPick === "P") {
         display("Fireball engulfs Paper");
         display("User Wins!");
+        userWins++;
       } if(compPick === "F") {
         display("Tied! User and Computer both picked Fireball.");
       }
@@ -92,13 +98,12 @@ function result() {
 }
 var result_div = document.getElementById('results');
 var startButton = document.createElement('button');
-  startButton.innerHTML = "start";
+  startButton.innerHTML = "Play";
   startButton.onclick = result;
   
   controls.appendChild(startButton);
 
 function display(mssg) {
-  // var result_div = document.getElementById('results');
   var result_p = document.createElement('p');
   result_p.innerHTML += mssg; 
   result_div.appendChild(result_p);
@@ -107,10 +112,6 @@ function display(mssg) {
 function clearDisplay () {
   result_div.innerHTML = "";
 }
-// do {
-//   result();
-// }
-// while();
 
 })();
 
